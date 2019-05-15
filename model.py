6e300df362c22097ff70d3a9fda65deecc1c6df4
Mohd Safwan <kdbeatbox@gmail.com>
2019-05-15 11:04:37 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,110 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras.activations import relu
+from tensorflow.keras.layers import *
+from tensorflow.keras import Model
+from tensorflow.keras import layers as Layers
+
+
+class ResBlock(Model):
+    def __init__(self, channels, flag=False):
+        super(ResBlock, self).__init__(name='ResBlock')
+        self.flag = flag
+        self.conv1 = Conv2D(channels, 3, padding='same')
+        self.bn1 = BatchNormalization()
+        self.conv2 = Conv2D(channels, 3, padding='same')
+        self.bn2 = BatchNormalization()
+        self.relu = ReLU()
+        if flag:
+            self.conv3 = Conv2D(channels, 1, 1)
+            self.bn3 = BatchNormalization()
+
+    def call(self, x):
+        x1 = self.conv1(x)
+        x1 = self.bn1(x1)
+        x1 = self.relu(x1)
+        x1 = self.conv2(x1)
+        x1 = self.bn2(x1)
+        if self.flag:
+            x = self.conv3(x)
+            x = self.bn3(x)
+        x1 = Layers.add([x, x1])
+        x1 = self.relu(x1)
+        return x1
+
+
+
+class PalmNet(Model):
+    def __init__(self):
+        super(PalmNet, self).__init__(name='PalmNet')
+        self.conv1 = Conv2D(64, 7, 2, padding='same')
+        self.bn = BatchNormalization()
+        self.relu = ReLU()
+        self.mp1 = MaxPooling2D(3, 2)
+
+        self.conv2_1 = ResBlock(64)
+        self.conv2_2 = ResBlock(64)
+        self.conv2_3 = ResBlock(64)
+
+        self.conv3_1 = ResBlock(128, True)
+        self.conv3_2 = ResBlock(128)
+        self.conv3_3 = ResBlock(128)
+        self.conv3_4 = ResBlock(128)
+
+        self.conv4_1 = ResBlock(256, True)
+        self.conv4_2 = ResBlock(256)
+        self.conv4_3 = ResBlock(256)
+        self.conv4_4 = ResBlock(256)
+        self.conv4_5 = ResBlock(256)
+        self.conv4_6 = ResBlock(256)
+
+        self.conv5_1 = ResBlock(512, True)
+        self.conv5_2 = ResBlock(512)
+        self.conv5_3 = ResBlock(512)
+
+        self.pool = GlobalAveragePooling2D()
+        self.fc1  = Dense(512, activation='relu')
+        self.dp1  = Dropout(0.5)
+        self.fc2  = Dense(512, activation='relu')
+        self.dp2  = Dropout(0.5)
+        self.fc3  = Dense(64)
+
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.mp1(x)
+
+        x = self.conv2_1(x)
+        x = self.conv2_2(x)
+        x = self.conv2_3(x)
+
+        x = self.conv3_1(x)
+        x = self.conv3_2(x)
+        x = self.conv3_3(x)
+        x = self.conv3_4(x)
+        
+        x = self.conv4_1(x)
+        x = self.conv4_2(x)
+        x = self.conv4_3(x)
+        x = self.conv4_4(x)
+        x = self.conv4_5(x)
+        x = self.conv4_6(x)
+        
+        x = self.conv5_1(x)
+        x = self.conv5_2(x)
+        x = self.conv5_3(x)
+
+        x = self.pool(x)
+        x = self.fc1(x)
+        x = self.dp1(x)
+        x = self.fc2(x)
+        x = self.dp2(x)
+        x = self.fc3(x)
+        return x
+
+
+model = PalmNet()
+model.build(input_shape=(1, 480, 480, 3))
+model.summary()
