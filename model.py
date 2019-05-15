29b74ef336e7c20eb047b639fedb3ff31b3dcd1d
Mohd Safwan <kdbeatbox@gmail.com>
2019-05-15 11:25:16 +0530

    Update model.py

--- model.py
+++ model.py
@@ -7,17 +7,17 @@ from tensorflow.keras import layers as Layers
 
 
 class ResBlock(Model):
-    def __init__(self, channels, flag=False):
+    def __init__(self, channels, stride=1):
         super(ResBlock, self).__init__(name='ResBlock')
-        self.flag = flag
-        self.conv1 = Conv2D(channels, 3, padding='same')
+        self.flag = (stride != 1)
+        self.conv1 = Conv2D(channels, 3, stride, padding='same')
         self.bn1 = BatchNormalization()
         self.conv2 = Conv2D(channels, 3, padding='same')
         self.bn2 = BatchNormalization()
         self.relu = ReLU()
-        if flag:
-            self.conv3 = Conv2D(channels, 1, 1)
+        if self.flag:
             self.bn3 = BatchNormalization()
+            self.conv3 = Conv2D(channels, 1, stride)
 
     def call(self, x):
         x1 = self.conv1(x)
@@ -30,13 +30,12 @@ class ResBlock(Model):
             x = self.bn3(x)
         x1 = Layers.add([x, x1])
         x1 = self.relu(x1)
-        return x1
+        return x7
 
 
-
-class ResNet(Model):
+class ResNet34(Model):
     def __init__(self):
-        super(ResNet, self).__init__(name='ResNet34')
+        super(ResNet34, self).__init__(name='ResNet34')
         self.conv1 = Conv2D(64, 7, 2, padding='same')
         self.bn = BatchNormalization()
         self.relu = ReLU()
@@ -46,29 +45,28 @@ class ResNet(Model):
         self.conv2_2 = ResBlock(64)
         self.conv2_3 = ResBlock(64)
 
-        self.conv3_1 = ResBlock(128, True)
+        self.conv3_1 = ResBlock(128, 2)
         self.conv3_2 = ResBlock(128)
         self.conv3_3 = ResBlock(128)
         self.conv3_4 = ResBlock(128)
 
-        self.conv4_1 = ResBlock(256, True)
+        self.conv4_1 = ResBlock(256, 2)
         self.conv4_2 = ResBlock(256)
         self.conv4_3 = ResBlock(256)
         self.conv4_4 = ResBlock(256)
         self.conv4_5 = ResBlock(256)
         self.conv4_6 = ResBlock(256)
 
-        self.conv5_1 = ResBlock(512, True)
+        self.conv5_1 = ResBlock(512, 2)
         self.conv5_2 = ResBlock(512)
         self.conv5_3 = ResBlock(512)
 
         self.pool = GlobalAveragePooling2D()
-        self.fc1  = Dense(512, activation='relu')
-        self.dp1  = Dropout(0.5)
-        self.fc2  = Dense(512, activation='relu')
-        self.dp2  = Dropout(0.5)
-        self.fc3  = Dense(64)
-
+        self.fc1 = Dense(512, activation='relu')
+        self.dp1 = Dropout(0.5)
+        self.fc2 = Dense(512, activation='relu')
+        self.dp2 = Dropout(0.5)
+        self.fc3 = Dense(64)
 
     def call(self, x):
         x = self.conv1(x)
@@ -84,14 +82,14 @@ class ResNet(Model):
         x = self.conv3_2(x)
         x = self.conv3_3(x)
         x = self.conv3_4(x)
-        
+
         x = self.conv4_1(x)
         x = self.conv4_2(x)
         x = self.conv4_3(x)
         x = self.conv4_4(x)
         x = self.conv4_5(x)
         x = self.conv4_6(x)
-        
+
         x = self.conv5_1(x)
         x = self.conv5_2(x)
         x = self.conv5_3(x)
@@ -105,6 +103,6 @@ class ResNet(Model):
         return x
 
 
-model = ResNet()
+model = ResNet34()
 model.build(input_shape=(1, 480, 480, 3))
 model.summary()
